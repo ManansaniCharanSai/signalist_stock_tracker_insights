4e5cadd0df34dd29f2b1126d583e479c421f5677
Manasani Charan Sai <2200032948@kluniversity.in>
2025-10-07 21:46:12 +0530

    implement header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from "@/components/ui/button";
+import Header from "@/components/Header";
+
+ const Home = () => {
+    return (
+        <div className=" flex min-h-screen home-wrapper">
+            <Header />
+        </div>
+    )
+}
+export default Home
